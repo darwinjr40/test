f0fab52629fde74b013d00f9e4912f0ac24714e8
Darwin-Mamani <darwinjr40@gmail.com>
2024-10-16 16:04:23 -0400

    feat: clear project

--- public/lib/environment.dart
+++ public/lib/environment.dart
@@ -1,10 +1,5 @@
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class Environment {
-  static String baseUrl = 'https://us-central1-shift-management-app-12ef1.cloudfunctions.net/api'; 
-  // static String baseUrl = dotenv.env['BASE_URL'] ?? 'https://us-central1-shift-management-app-12ef1.cloudfunctions.net/api'; 
-  // static String path = dotenv.env['PATH'] ?? '';
-  // static String apiUrl = dotenv.env['API_URL'] ?? '';
-  // static String url = dotenv.env['URL'] ?? '';
-  // static String webUrl = dotenv.env['WEB_URL'] ?? '';
+class Environment {  
+  static String baseUrl = ''; 
 }
